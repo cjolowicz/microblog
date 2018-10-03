2f4dc3cee23fd21c04d93a080bb6ca1d0a68a7b7
Claudio Jolowicz <mail@claudiojolowicz.com>
2018-10-03 16:43:35 +0200

    Use avatar URLs.

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from hashlib import md5
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from app import db, login
@@ -22,6 +23,11 @@ class User(UserMixin, db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
+            digest, size)
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)

3f0d81a62cf14be736d9e0c7d0e24b78f1ce66b5
Claudio Jolowicz <mail@claudiojolowicz.com>
2018-10-03 15:49:04 +0200

    Redirect to next page.

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,5 @@
-from flask import render_template, flash, redirect, url_for
+from werkzeug.urls import url_parse
+from flask import render_template, flash, redirect, url_for, request
 from flask_login import current_user, login_user, logout_user, login_required
 from app import app
 from app.forms import LoginForm
@@ -32,7 +33,10 @@ def login():
             flash('Invalid username or password')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
-        return redirect(url_for('index'))
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
 
 @app.route('/logout')

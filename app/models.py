78ad9eff5edb7c71207fd91a7e7a02c7383c054c
Claudio Jolowicz <mail@claudiojolowicz.com>
2018-10-03 20:47:10 +0200

    Add followers association table.

--- app/models.py
+++ app/models.py
@@ -8,6 +8,11 @@ from app import db, login
 def load_user(id):
     return User.query.get(int(id))
 
+followers = db.Table('followers',
+    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+)
+
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)

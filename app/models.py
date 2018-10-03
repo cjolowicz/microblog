53c114bde33ce8a2944cbad1bb5ee6b4d05ce643
Claudio Jolowicz <mail@claudiojolowicz.com>
2018-10-03 21:58:33 +0200

    Add many-to-many followers relationship.

--- app/models.py
+++ app/models.py
@@ -21,6 +21,11 @@ class User(UserMixin, db.Model):
     posts = db.relationship('Post', backref='author', lazy='dynamic')
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
+    followed = db.relationship(
+        'User', secondary=followers,
+        primaryjoin=(followers.c.follower_id == id),
+        secondaryjoin=(followers.c.followed_id == id),
+        backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
 
     def __repr__(self):
         return '<User {}>'.format(self.username)

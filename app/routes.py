203db01ca67e5c6d57dbe9ec5f6773182eb0d71b
Claudio Jolowicz <mail@claudiojolowicz.com>
2018-10-03 15:51:12 +0200

    Do not pass user to template anymore.

--- app/routes.py
+++ app/routes.py
@@ -9,7 +9,6 @@ from app.models import User
 @app.route('/index')
 @login_required
 def index():
-    user = {'username': 'Miguel'}
     posts = [
         {
             'author': {'username': 'John'},
@@ -20,7 +19,7 @@ def index():
             'body': 'The Avengers movie was so cool!'
         }
     ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    return render_template('index.html', title='Home', posts=posts)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():

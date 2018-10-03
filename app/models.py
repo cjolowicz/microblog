76595b431f3520554c3cf9fff39e5631bc8d0a55
Claudio Jolowicz <mail@claudiojolowicz.com>
2018-10-03 15:13:44 +0200

    Add Flask-Login user mixin class.

--- app/models.py
+++ app/models.py
@@ -1,8 +1,9 @@
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 from app import db
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)

6583a6da55de0583fbeaf2f5baeaf4b7cf6c9bbc
Claudio Jolowicz <mail@claudiojolowicz.com>
2018-10-03 15:44:24 +0200

    Use login_required decorator on index view function.

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,12 @@
 from flask import render_template, flash, redirect, url_for
-from flask_login import current_user, login_user, logout_user
+from flask_login import current_user, login_user, logout_user, login_required
 from app import app
 from app.forms import LoginForm
 from app.models import User
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
     user = {'username': 'Miguel'}
     posts = [

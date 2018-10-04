55b8d5f4e90994060ca7553e290d5fb6adb4d3d8
Claudio Jolowicz <mail@claudiojolowicz.com>
2018-10-04 21:14:11 +0200

    Store select language in flask.g.

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,8 @@
 from datetime import datetime
 from werkzeug.urls import url_parse
-from flask import render_template, flash, redirect, url_for, request
+from flask import render_template, flash, redirect, url_for, request, g
 from flask_login import current_user, login_user, logout_user, login_required
+from flask_babel import get_locale
 from app import app, db
 from app.forms import (LoginForm, RegistrationForm, EditProfileForm, PostForm,
                        ResetPasswordRequestForm, ResetPasswordForm)
@@ -13,6 +14,7 @@ def before_request():
     if current_user.is_authenticated:
         current_user.last_seen = datetime.utcnow()
         db.session.commit()
+    g.locale = str(get_locale())
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])

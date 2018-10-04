c2d67dd65e17b4966eb99af285c758f620abb804
Claudio Jolowicz <mail@claudiojolowicz.com>
2018-10-04 18:39:48 +0200

    Add reset password token methods.

--- app/models.py
+++ app/models.py
@@ -1,8 +1,10 @@
+from time import time
 from datetime import datetime
 from hashlib import md5
+import jwt
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
-from app import db, login
+from app import app, db, login
 
 @login.user_loader
 def load_user(id):
@@ -35,6 +37,20 @@ class User(UserMixin, db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    def get_reset_password_token(self, expires_in=600):
+        return jwt.encode(
+            {'reset_password': self.id, 'exp': time() + expires_in},
+            app.config['SECRET_KEY'], algorithm='HS256').decode('utf-8')
+
+    @staticmethod
+    def verify_reset_password_token(token):
+        try:
+            id = jwt.decode(token, app.config['SECRET_KEY'],
+                            algorithms=['HS256'])['reset_password']
+        except:
+            return
+        return User.query.get(id)
 
     def avatar(self, size):
         digest = md5(self.email.lower().encode('utf-8')).hexdigest()

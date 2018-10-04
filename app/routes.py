858c483e8a8ec43aee61d6e5df6c4f3388575a80
Claudio Jolowicz <mail@claudiojolowicz.com>
2018-10-04 14:47:35 +0200

    Display real posts on home page.

--- app/routes.py
+++ app/routes.py
@@ -23,16 +23,7 @@ def index():
         db.session.commit()
         flash('Your post is now live!')
         return redirect(url_for('index'))
-    posts = [
-        {
-            'author': {'username': 'John'},
-            'body': 'Beautiful day in Portland!'
-        },
-        {
-            'author': {'username': 'Susan'},
-            'body': 'The Avengers movie was so cool!'
-        }
-    ]
+    posts = current_user.followed_posts().all()
     return render_template('index.html', title='Home Page', form=form,
                            posts=posts)
 

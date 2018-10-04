fe322e4364c2ce84c6d55c916fcfd76d6f47adf9
Claudio Jolowicz <mail@claudiojolowicz.com>
2018-10-04 22:17:34 +0200

    Add init sub-command.

--- app/cli.py
+++ app/cli.py
@@ -1,4 +1,5 @@
 import os
+import click
 from app import app
 
 @app.cli.group()
@@ -20,3 +21,14 @@ def compile():
     """Compile all languages."""
     if os.system('pybabel compile -d app/translations'):
         raise RuntimeError('compile command failed')
+
+@translate.command()
+@click.argument('lang')
+def init(lang):
+    """Initialize a new language."""
+    if os.system('pybabel extract -F babel.cfg -k _l -o messages.pot .'):
+        raise RuntimeError('extract command failed')
+    if os.system(
+            'pybabel init -i messages.pot -d app/translations -l ' + lang):
+        raise RuntimeError('init command failed')
+    os.remove('messages.pot')

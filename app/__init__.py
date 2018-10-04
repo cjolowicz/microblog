c4f544cd1a1fe6f1947140ed0cc6c164e828e213
Claudio Jolowicz <mail@claudiojolowicz.com>
2018-10-04 20:57:41 +0200

    Add Flask-Babel instance.

--- app/__init__.py
+++ app/__init__.py
@@ -9,6 +9,7 @@ from flask_login import LoginManager
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_babel import Babel
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -19,6 +20,7 @@ login.login_view = 'login'
 mail = Mail(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+babel = Babel(app)
 
 if not app.debug:
     if app.config['MAIL_SERVER']:

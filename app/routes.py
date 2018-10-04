3841177eabdeedacd475a3db74e28ccb730d4a99
Claudio Jolowicz <mail@claudiojolowicz.com>
2018-10-04 22:36:05 +0200

    Save language for new posts.

--- app/routes.py
+++ app/routes.py
@@ -3,6 +3,7 @@ from werkzeug.urls import url_parse
 from flask import render_template, flash, redirect, url_for, request, g
 from flask_login import current_user, login_user, logout_user, login_required
 from flask_babel import _, get_locale
+from guess_language import guess_language
 from app import app, db
 from app.forms import (LoginForm, RegistrationForm, EditProfileForm, PostForm,
                        ResetPasswordRequestForm, ResetPasswordForm)
@@ -22,7 +23,11 @@ def before_request():
 def index():
     form = PostForm()
     if form.validate_on_submit():
-        post = Post(body=form.post.data, author=current_user)
+        language = guess_language(form.post.data)
+        if language == 'UNKNOWN' or len(language) > 5:
+            language = ''
+        post = Post(body=form.post.data, author=current_user,
+                    language=language)
         db.session.add(post)
         db.session.commit()
         flash(_('Your post is now live!'))

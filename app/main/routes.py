1f13aa282e68b5c5baa5e99e05ad09183a05554a
Claudio Jolowicz <mail@claudiojolowicz.com>
2018-10-05 19:40:41 +0200

    Instantiate the search form in the before request handler.

--- app/main/routes.py
+++ app/main/routes.py
@@ -5,7 +5,7 @@ from flask_login import current_user, login_required
 from flask_babel import _, get_locale
 from guess_language import guess_language
 from app import db
-from app.main.forms import EditProfileForm, PostForm
+from app.main.forms import EditProfileForm, PostForm, SearchForm
 from app.models import User, Post
 from app.translate import translate
 from app.main import bp
@@ -16,6 +16,7 @@ def before_request():
     if current_user.is_authenticated:
         current_user.last_seen = datetime.utcnow()
         db.session.commit()
+        g.search_form = SearchForm()
     g.locale = str(get_locale())
 
 

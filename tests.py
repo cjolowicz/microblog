3c9e55cdf977c64c9264366b11a84fb7f6b97e1a
Claudio Jolowicz <mail@claudiojolowicz.com>
2018-10-05 15:49:50 +0200

    Create an application for each test.

--- tests.py
+++ tests.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 from datetime import datetime, timedelta
 import unittest
-from app import app, db
+from app import create_app, db
 from app.models import User, Post
 from config import Config
 
@@ -11,12 +11,15 @@ class TestConfig(Config):
 
 class UserModelCase(unittest.TestCase):
     def setUp(self):
-        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://'
+        self.app = create_app(TestConfig)
+        self.app_context = self.app.app_context()
+        self.app_context.push()
         db.create_all()
 
     def tearDown(self):
         db.session.remove()
         db.drop_all()
+        self.app_context.pop()
 
     def test_password_hashing(self):
         u = User(username='susan')

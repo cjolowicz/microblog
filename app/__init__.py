93222af2df112b4c165f54b46942b6d9a4434313
Claudio Jolowicz <mail@claudiojolowicz.com>
2018-10-05 15:29:49 +0200

    Allow passing a different configuration class to the application factory function.

--- app/__init__.py
+++ app/__init__.py
@@ -21,9 +21,9 @@ bootstrap = Bootstrap()
 moment = Moment()
 babel = Babel()
 
-def create_app():
+def create_app(config_class=Config):
     app = Flask(__name__)
-    app.config.from_object(Config)
+    app.config.from_object(config_class)
 
     db.init_app(app)
     migrate.init_app(app, db)

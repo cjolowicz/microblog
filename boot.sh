d607075867feb4b9a3357902fd80faf9dfd261a3
Claudio Jolowicz <mail@claudiojolowicz.com>
2018-10-06 22:54:46 +0200

    Retry database access in boot script.

--- boot.sh
+++ boot.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
+
 source venv/bin/activate
-flask db upgrade
+
+while true; do
+    flask db upgrade
+    if [[ $? == 0 ]]; then
+        break
+    fi
+    echo 'Upgrade command failed, retrying in 5 secs...'
+    sleep 5
+done
+
 flask translate compile
 exec gunicorn -b :5000 --access-logfile - --error-logfile - microblog:app

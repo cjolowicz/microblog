1b11073c355027d4ba341a892a0043901a4fc281
Claudio Jolowicz <mail@claudiojolowicz.com>
2018-10-06 16:50:08 +0200

    Add option to log to stdout.

--- config.py
+++ config.py
@@ -20,3 +20,4 @@ class Config(object):
     MS_TRANSLATOR_KEY = os.environ.get('MS_TRANSLATOR_KEY')
     POSTS_PER_PAGE = 25
     ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL')
+    LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')

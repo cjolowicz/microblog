3f7a1b581c2de6ffc226882876eafb5c43d8b32f
Claudio Jolowicz <mail@claudiojolowicz.com>
2018-10-07 14:18:20 +0200

    Pass environment variables.

--- docker-run.sh
+++ docker-run.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+SECRET_KEY=33b7497205884cd2801a03ac16109409
+
 docker run --name microblog \
        --detach \
        --publish 8000:5000 \
        --rm \
+       --env SECRET_KEY=$SECRET_KEY \
        microblog:latest

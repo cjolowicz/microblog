72bac19115afdaa79fc1380110d2f6bbb2fa1643
Claudio Jolowicz <mail@claudiojolowicz.com>
2018-10-07 14:47:54 +0200

    Revert "Allow posts of any size."
    This reverts commit 042f805eef3258dc9f474f0c88ce6ed5d5330270.

--- app/main/forms.py
+++ app/main/forms.py
@@ -24,7 +24,7 @@ class EditProfileForm(FlaskForm):
 
 
 class PostForm(FlaskForm):
-    post = TextAreaField(_l('Say something'), validators=[DataRequired()])
+    post = TextAreaField(_l('Say something'), validators=[DataRequired(), Length(min=1, max=140)])
     submit = SubmitField(_l('Submit'))
 
 

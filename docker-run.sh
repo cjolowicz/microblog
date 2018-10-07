178e486104e983d9eabd03e58022e81a4178caa6
Claudio Jolowicz <mail@claudiojolowicz.com>
2018-10-07 23:48:52 +0200

    Use named volume for mysql database.

--- docker-run.sh
+++ docker-run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+MYSQL_VOLUME=microblog-mysql-volume
 MYSQL_DATABASE=microblog
 MYSQL_USER=microblog
 MYSQL_PASSWORD=a737e6b789a04be6a15c73d43a83b967
@@ -9,13 +10,17 @@ SECRET_KEY=33b7497205884cd2801a03ac16109409
 DATABASE_URL=mysql+pymysql://$MYSQL_USER:$MYSQL_PASSWORD@mysql/$MYSQL_DATABASE
 ELASTICSEARCH_URL=http://elasticsearch:9200
 
+
 # Start mysql container.
+docker volume ls -q | fgrep -q $MYSQL_VOLUME ||
+    docker volume create --name $MYSQL_VOLUME
 docker run --name mysql \
        --detach \
        --env MYSQL_RANDOM_ROOT_PASSWORD=$MYSQL_RANDOM_ROOT_PASSWORD \
        --env MYSQL_DATABASE=$MYSQL_DATABASE \
        --env MYSQL_USER=$MYSQL_USER \
        --env MYSQL_PASSWORD=$MYSQL_PASSWORD \
+       --volume $MYSQL_VOLUME:/var/lib/mysql \
        mysql/mysql-server:5.7
 
 # Start elasticsearch container.

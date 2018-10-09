eb36894f945e0e317e4942b251c4c9bb25e3e85f
Claudio Jolowicz <mail@claudiojolowicz.com>
2018-10-09 15:11:50 +0200

    Update user notification in send_message route.

--- app/main/routes.py
+++ app/main/routes.py
@@ -166,6 +166,7 @@ def send_message(recipient):
         msg = Message(author=current_user, recipient=user,
                       body=form.message.data)
         db.session.add(msg)
+        user.add_notification('unread_message_count', user.new_messages())
         db.session.commit()
         flash(_('Your message has been sent.'))
         return redirect(url_for('main.user', username=recipient))
